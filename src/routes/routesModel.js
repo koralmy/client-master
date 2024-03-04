f44ef8f7fb64f1319bab6e6753178514ae12c6f3
koralmy <koralstudent@gmail.com>
2024-03-04 22:28:39 +0200

    .

--- src/routes/routesModel.js
+++ src/routes/routesModel.js
@@ -2,13 +2,13 @@ const ROUTES = {
   CARDS: "https://koralmy.github.io/client-master",
   ABOUT: "https://koralmy.github.io/client-master/about",
   MY_CARDS: "https://koralmy.github.io/client-master/my-cards",
-  FAV_CARDS: "/fav-cards",
-  CARD_DETAILS: "/card-info",
-  SANDBOX: "/sandbox",
-  LOGIN: "/login",
-  SIGNUP: "/signup",
-  CREATE_CARD: "/create-card",
-  EDIT_CARD: "/edit-card",
+  FAV_CARDS: "https://koralmy.github.io/client-master/fav-cards",
+  CARD_DETAILS: "https://koralmy.github.io/client-master/card-info",
+  SANDBOX: "https://koralmy.github.io/client-master/sandbox",
+  LOGIN: "https://koralmy.github.io/client-master/login",
+  SIGNUP: "https://koralmy.github.io/client-master/signup",
+  CREATE_CARD: "https://koralmy.github.io/client-master/create-card",
+  EDIT_CARD: "https://koralmy.github.io/client-master/edit-card",
 };
 
 export default ROUTES;
